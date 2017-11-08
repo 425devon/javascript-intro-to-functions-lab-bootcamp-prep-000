e591e7a3529b1118a22e7701c841fe88f42a7281
Devon Langendoerfer <425devon@gmail.com>
2017-11-08 20:38:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   var speak = string;
-  if(speak.toLowerCase === string){
+  if(speak === string.toLowerCase()){
     return "I can\'t hear you!";
-  }else if (speak.toUpperCase === string) {
+  }else if (speak === string.toUpperCase()) {
     return "YES INDEED!";
 
   }else if(string === "I love you, Grandma."){
